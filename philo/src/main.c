8c52c6372f544110294856190a00ac1df33f6365
Hugo Lopez <hlopez@z3r3p4.42lyon.fr>
2024-02-28 15:32:49 +0100

    updated main

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: hlopez <hlopez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 11:58:41 by hlopez            #+#    #+#             */
-/*   Updated: 2024/02/28 13:17:06 by hlopez           ###   ########.fr       */
+/*   Updated: 2024/02/28 15:27:20 by hlopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void	print_param_error(void)
 {
 	printf("Incorrect parameters. Command should be as follows:\n");
-	printf("./philo <number_of_philosophers> <time_to_die> <time_to_eat> ");
+	printf("./philo <number_of_philos> <time_to_die> <time_to_eat> ");
 	printf("<time_to_sleep> [<number_of_times_each_philosopher_must_eat>]\n");
 	printf("(Last one is optional.)\n");
 }
@@ -26,6 +26,8 @@ static int	check_params(int ac, char **av)
 	size_t	j;
 
 	i = 0;
+	if (ac != 5 && ac != 6)
+		return (0);
 	while (++i < ac)
 	{
 		j = 0;
@@ -44,13 +46,13 @@ void	test_print(t_diner *d)
 {
 	int		i;
 
-	printf("Number of philosophers : %d\n", d->number_of_philosophers);
+	printf("Number of philosophers : %d\n", d->number_of_philos);
 	printf("Time to die : %d\n", d->time_to_die);
 	printf("Time to eat : %d\n", d->time_to_eat);
 	printf("Time to sleep : %d\n", d->time_to_sleep);
 	i = 0;
 	printf("Existing philosophers :\n");
-	while (i < d->number_of_philosophers)
+	while (i < d->number_of_philos)
 	{
 		printf("Philosopher number %d\n", d->ph[i]->number);
 		i++;
@@ -66,7 +68,7 @@ void	ft_free(t_diner *d)
 	{
 		if (d->ph)
 		{
-			while (i < d->number_of_philosophers)
+			while (i < d->number_of_philos)
 			{
 				if (d->ph[i])
 					free(d->ph[i]);
@@ -82,7 +84,7 @@ int	main(int ac, char **av)
 {
 	t_diner	*d;
 
-	if ((ac == 5 || ac == 6) && check_params(ac, av))
+	if (check_params(ac, av))
 	{
 		d = (t_diner *)malloc(sizeof(t_diner));
 		if (!d)
