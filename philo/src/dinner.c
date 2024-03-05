8abbfcf958d1c1166bca61adacdc17fe536e4d5e
Hugo Lopez <hlopez@z4r6p2.42lyon.fr>
2024-03-05 14:22:55 +0100

    renamed dinner.c

--- philo/src/dinner.c
+++ philo/src/dinner.c
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   diner.c                                            :+:      :+:    :+:   */
+/*   dinner.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlopez <hlopez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 16:47:50 by hlopez            #+#    #+#             */
-/*   Updated: 2024/02/28 16:52:10 by hlopez           ###   ########.fr       */
+/*   Updated: 2024/03/05 13:49:43 by hlopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*ft_dining(t_diner *d)
+static void	wait_all_threads(t_dinner *d)
 {
 	
+}
+
+void	*ft_dining(void *data)
+{
+	t_dinner	*d;
+
+	d = (t_dinner *)data;
+	wait_all_threads(d);
 	return (NULL);
 }
